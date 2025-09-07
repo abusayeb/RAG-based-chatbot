ad5d662be0bb59f82824b9f8b1c0bfb837dee8cd
abusayeb <sayeb.cc.75@gmail.com>
2025-09-07 20:44:16 +0900

    Template is ready

---
+++ template.sh
@@ -0,0 +1,15 @@
+mkdir -p src
+mkdir -p testing
+
+
+touch testing/test.ipynb
+touch src/__init__.py
+touch src/prompt.py
+touch src/utils.py
+touch .env
+touch setup.py
+touch app.py
+touch requirements.txt
+
+echo " Template files created successfully."
+
