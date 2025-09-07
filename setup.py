db3867c4e26003a82d826ceb2b7dffbd75748ff4
abusayeb <sayeb.cc.75@gmail.com>
2025-09-07 21:27:14 +0900

    requirements changed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "Chatbot",
+    version = "0.1.0",
+    author = "Abu Sayeb Rayhan",
+    author_email= "sayeb.cc.75@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+)
